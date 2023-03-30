56047ed1fc52be8d6447dd44ce09594e56669d6f
Shane <phes10217@gmail.com>
2023-03-30 13:27:21 +0800

    chore: renew models

--- models/transaction.js
+++ models/transaction.js
@@ -1,10 +1,10 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
     const Transaction = sequelize.define('Transaction', {
         name: DataTypes.STRING,
         date: DataTypes.DATEONLY,
         amount: DataTypes.INTEGER,
-        type: DataTypes.ENUM
+        type: DataTypes.ENUM('income', 'expense')
     }, {
         underscored: true, // 將模型的屬性名稱改為底線格式
         timestamps: true
@@ -13,5 +13,5 @@ module.exports = (sequelize, DataTypes) => {
         // associations can be defined here
         Transaction.belongsTo(models.User, { foreignKey: 'UserId' })
     };
-    return Transaction;
-};
+    return Transaction
+}
--- models/user.js
+++ models/user.js
@@ -1,11 +1,9 @@
-'use strict';
+'use strict'
 module.exports = (sequelize, DataTypes) => {
     const User = sequelize.define('User', {
         name: DataTypes.STRING,
         email: DataTypes.STRING,
         password: DataTypes.STRING,
-        isAdmin: DataTypes.BOOLEAN,
-        image: DataTypes.STRING
     }, {
         underscored: true, // 將模型的屬性名稱改為底線格式
         timestamps: true
@@ -13,10 +11,7 @@ module.exports = (sequelize, DataTypes) => {
     User.associate = function (models) {
         // 和Transaction的關聯
         User.hasMany(models.Transaction, { foreignKey: 'UserId' })
-        // 和Reminder的關聯
-        User.hasMany(models.Reminder, { foreignKey: 'UserId' })
-        // 和Budget的關聯
-        User.hasMany(models.Budget, { foreignKey: 'UserId' })
-    };
-    return User;
-};
+    }
+    return User
+}
+
