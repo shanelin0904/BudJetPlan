fe6719d284de96875726f6960f25b12b4693e595
Shane <phes10217@gmail.com>
2023-04-06 14:40:18 +0800

    feat: Add error-handler to register

--- routes/modules/sign.js
+++ routes/modules/sign.js
@@ -3,8 +3,8 @@ const router = express.Router()
 const passport = require('passport')
 const userController = require('../../controllers/userController')
 const { loginFail } = require('../../middleware/error-handler')
-
-router.post('/register', userController.register)
+const {  errorHandler } = require('../../middleware/error-handler')
+router.post('/register', userController.register, errorHandler)
 router.post('/login', passport.authenticate('local', { session: false, failWithError: true }), userController.login, loginFail)
 router.post('/logout', userController.logout)
 module.exports = router
